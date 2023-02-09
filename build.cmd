708fcd0d570c709077c6b0bac6dd144d65c5726a
Alphons van der Heijden <alphons@alphons>
2023-02-09 13:50:27 +0000

    init

---
+++ build.cmd
@@ -0,0 +1,63 @@
+@echo off
+rem
+rem (C) 2023 Alphons van der Heijden
+rem 
+rem Create a bootable disk out of kernel and initrd
+rem for VMWARE player/esxi without bootloader
+rem using nasm creating a bootsector
+rem creating a vmdk file
+rem 
+rem
+setlocal
+
+set OUTPUT=linux-6.1.2
+set KERNEL=vmlinuz64
+set INITRD=corepure64.gz
+set CMDLINE='loglevel=3'
+
+rem now everything is configured, lets build the bootable disk and vmdk file
+
+call :getsize %kernel%
+set K_SZ=%size%
+call :getsize %initrd%
+set R_SZ=%size%
+set S=512
+set /a K_PAD=512-K_SZ%%S
+set /a R_PAD=512-R_SZ%%s
+
+nasm -o bsect.bin -D initRdSizeDef=%R_SZ% -D cmdLineDef=%CMDLINE% bsect.asm
+fsutil file createnew "k_pad.bin" %K_PAD% > NUL
+fsutil file createnew "r_pad.bin" %R_PAD% > NUL
+
+copy bsect.bin /B + %KERNEL% /B + k_pad.bin /B + %INITRD% /B + r_pad.bin / B %OUTPUT% /B > NUL
+del k_pad.bin
+del r_pad.bin
+del bsect.bin
+
+call :getsize %OUTPUT%
+set TOTAL=%size%
+set /a SECTORS=%TOTAL% / 512
+set /a CYLINDERS=%SECTORS% / 16065
+
+(
+echo version=1
+echo encoding="UTF-8"
+echo CID=123456789
+echo parentCID=ffffffff
+echo createType="vmfs"
+echo RW %SECTORS% VMFS "%OUTPUT%"
+echo ddb.virtualHWVersion = "8"
+echo ddb.geometry.cylinders = "%CYLINDERS%"
+echo ddb.geometry.heads = "255"
+echo ddb.geometry.sectors = "63"
+echo ddb.adapterType = "lsilogic"
+)>%OUTPUT%.vmdk
+
+echo bootsector, kernel and initrd catenated in %OUTPUT% and created %OUTPUT%.vmdk
+pause
+
+goto :eof
+
+:getsize
+set size=%~z1
+goto :eof
---
+++ build.sh
@@ -0,0 +1,67 @@
+#!/bin/bash -e
+# Tiny Linux Bootloader
+# (c) 2014- Dr Gareth Owen (www.ghowen.me). All rights reserved.
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    Changed by Alphons van der Heijden for making .vmdk disk for booting in vmware
+
+WORK=/mnt/sda1/release/iso_contents/boot
+
+OUTPUT="linux-6.1.2"
+KERNEL="$WORK/vmlinuz64"
+INITRD="$WORK/corepure64.gz"
+CMDLINE="'loglevel=3'"
+
+#bootsector assembly
+BOOT="bsect.asm"
+
+#size of kern + ramdisk
+K_SZ=$(stat -c %s $KERNEL)
+R_SZ=$(stat -c %s $INITRD)
+
+#padding to make it up to a sector
+K_PAD=$((512 - $K_SZ % 512))
+R_PAD=$((512 - $R_SZ % 512))
+
+nasm -o $OUTPUT -D initRdSizeDef=$R_SZ -D cmdLineDef=$CMDLINE $BOOT
+
+cat $KERNEL >> $OUTPUT
+if [[ $K_PAD -lt 512 ]]; then
+    dd if=/dev/zero bs=1 count=$K_PAD status=none >> $OUTPUT
+fi
+
+cat $INITRD >> $OUTPUT
+if [[ $R_PAD -lt 512 ]]; then
+    dd if=/dev/zero bs=1 count=$R_PAD status=none >> $OUTPUT
+fi
+
+TOTAL=$(stat -c %s $OUTPUT)
+SECTORS=$(($TOTAL / 512))
+CYLINDERS=$(($SECTORS / 16065))
+
+cat > $OUTPUT.vmdk <<EOF
+version=1
+encoding="UTF-8"
+CID=123456789
+parentCID=ffffffff
+createType="vmfs"
+RW $SECTORS VMFS "$OUTPUT"
+ddb.virtualHWVersion = "8"
+ddb.geometry.cylinders = "$CYLINDERS"
+ddb.geometry.heads = "255"
+ddb.geometry.sectors = "63"
+ddb.adapterType = "lsilogic"
+EOF
+
+echo "bootsector, kernel and initrd catenated in $OUTPUT and created $OUTPUT.vmdk"
